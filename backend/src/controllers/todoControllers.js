1ab9278bb976a87798611198bcebb9ad735b98df
loicWCS <loic.lebon.wcs@gmail.com>
2022-09-05 10:47:50 +0200

    CRUD is done

---
+++ backend/src/controllers/todoControllers.js
@@ -0,0 +1,75 @@
+const { sqlDb } = require("../../db");
+
+const getTodos = (req, res) => {
+  sqlDb
+    .query(`SELECT * FROM todos`)
+    .then(([result]) => {
+      // console.warn(result);
+      res.status(200).json({ result });
+    })
+    .catch((err) => {
+      // console.warn("There was an error in getTodos function", err);
+      res.status(500).send("Error retrieving geTodos", err);
+    });
+};
+
+const postTodos = (req, res) => {
+  const { description } = req.body;
+  sqlDb
+    .query("INSERT INTO todos (description) VALUES (?)", [description])
+    .then(([result]) => {
+      res.status(201).json({ id: result.insertId });
+    })
+    .catch((err) => {
+      res.status(500).send("Error inserting posTodos", err);
+    });
+};
+const deleteTodos = (req, res) => {
+  let { id } = req.body; // ou req.params si router.delete avec :id
+  id = parseInt(id, 10); // ou Number(id)
+  sqlDb
+    .query("DELETE FROM todos WHERE id = ?", [id]) // il s'agit d'une requete preparer qui evite les injections SQL
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res
+          .status(404)
+          .json({ message: `task was not found in DB because wrong id` });
+      } else {
+        res.status(201).json({ message: `task ${id} was  deleted ` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `task ${id} was not deleted because of error, ${err}`,
+      });
+    });
+};
+
+const updateTodos = (req, res) => {
+  let { id } = req.body; // ou req.params si router.delete avec :id
+  id = parseInt(id, 10); // ou Number(id)
+  const { description } = req.body;
+  sqlDb
+    .query("UPDATE todos SET description = ? WHERE id = ?", [description, id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res
+          .status(404)
+          .json({ message: `task was not found in DB because wrong id` });
+      } else {
+        res.status(201).json({ message: `task ${id} was  updated ` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: `task ${id} was not updated because of error, ${err}`,
+      });
+    });
+};
+
+module.exports = {
+  getTodos,
+  postTodos,
+  updateTodos,
+  deleteTodos,
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,13 +1,20 @@
+// router.js = vue (mvc)
 const express = require("express");
 
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const todoControllers = require("./controllers/todoControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/todos", todoControllers.getTodos);
+router.post("/todos", todoControllers.postTodos);
+router.put("/todos", todoControllers.updateTodos); // router.put("/todos/:id", todoControllers.updateTodos);
+router.delete("/todos", todoControllers.deleteTodos); // ou router.delete("/todos/:id", todoControllers.deleteTodos);
 
 module.exports = router;
